7f0d0f885c4ddc13a0077bdca181e7cfcda7f90d
Phaniraj35 <phaniitng@gmail.com>
2019-06-17 16:46:27 +0530

    Update ImportInterface.php

--- src/ImportInterface.php
+++ src/ImportInterface.php
@@ -5,6 +5,6 @@ use Illuminate\Http\Request;
 interface ImportInterface
 {
     public function parseImport(Request $request);
-    //public function processImport(Request $request);
+    public function processImport(Request $request);
     public function getDbCols();
 }
